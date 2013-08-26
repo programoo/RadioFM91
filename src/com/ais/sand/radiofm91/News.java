4dd7b9db641fb9d6022b261154b183743fb8d954
programoo <mevtwifu@gmail.com>
2013-08-26 17:40:34 +0700

    daily update

---
+++ src/com/ais/sand/radiofm91/News.java
@@ -0,0 +1,55 @@
+package com.ais.sand.radiofm91;
+
+public class News {
+	private String tag = getClass().getSimpleName();
+	int id;
+	String type;
+	String primarySource;
+	String secondarySource;
+	String startTime;
+	String endTime;
+	String mediaType;
+	String path;
+	String title;
+	String description;
+	String locationType;
+	String roadName;
+	String startPoint;
+	String startPointLat, startPointLong;
+	String endPoint;
+	String endPointLat, endPointLong;
+
+	public News(int id, String type, String primarySource,
+			String secondarySource, String startTime, String endTime,
+			String mediaType, String path, String title, String description,
+			String locationType, String roadName, String startPoint,
+			String startPointLat, String startPointLong, String endPoint,
+			String endPointLat, String endPointLong) {
+		this.id = id;
+		this.type = type;
+		this.primarySource = primarySource;
+		this.secondarySource = secondarySource;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.mediaType = mediaType;
+		this.path = path;
+		this.title = title;
+		this.description = description;
+		this.locationType = locationType;
+		this.roadName = roadName;
+		this.startPoint = startPoint;
+		this.startPointLat = startPointLat;
+		this.startPointLong = startPointLong;
+		this.endPoint = endPoint;
+		this.endPointLat = endPointLat;
+		this.endPointLong = endPointLong;
+
+	}
+
+	@Override
+	public String toString() {
+		return id+","+type+","+primarySource+","+secondarySource+","+startTime+","+
+				endTime+","+mediaType+","+path+","+title+","+description+","+locationType+","+
+				roadName+","+startPoint+","+startPointLat+","+startPointLong+","+endPoint+endPointLat+","+endPointLong;
+	}
+}
--- src/com/ais/sand/radiofm91/RequestTask.java
+++ src/com/ais/sand/radiofm91/RequestTask.java
@@ -2,10 +2,15 @@ package com.ais.sand.radiofm91;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.StringReader;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -14,6 +19,13 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.w3c.dom.CharacterData;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import android.os.AsyncTask;
 import android.util.Log;
@@ -61,25 +73,21 @@ public class RequestTask extends AsyncTask<String, String, String> {
 
 		// Do anything with response..
 		if (randomStr == "undefined") {
-			// keep randomStr
 			randomStr = result;
-			// step2 create session key
 			String passKey = "";
 			try {
 				passKey = convertToMD5(AppID + randomStr)
 						+ convertToMD5(hiddenkey + randomStr);
 			} catch (NoSuchAlgorithmException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
-			// step3 url
 			String traffy_request_url = "http://api.traffy.in.th/apis/apitraffy.php?api=getIncident&key="
 					+ passKey + "&format=XML";
-			//request again for get real data from traffy
-			new RequestTask()
-			.execute(traffy_request_url);
-
+			new RequestTask().execute(traffy_request_url);
+		} else {
+			// this mean we get real data from traffy already
+			this.xmlParser(result);
 		}
 
 	}
@@ -98,5 +106,50 @@ public class RequestTask extends AsyncTask<String, String, String> {
 		}
 
 		return hashtext;
+	}
+
+	public void xmlParser(String result) {
+		String xmlRecords = "<data><employee><name>A</name>"
+				+ "<title>Manager</title></employee></data>";
+
+		DocumentBuilder db;
+		NodeList nodes = null;
+		try {
+			db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			InputSource is = new InputSource();
+			is.setCharacterStream(new StringReader(xmlRecords));
+
+			Document doc = db.parse(is);
+			nodes = doc.getElementsByTagName("employee");
+
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Element element = (Element) nodes.item(i);
+
+			NodeList name = element.getElementsByTagName("news");
+			Element line = (Element) name.item(0);
+			System.out.println("news: " + getCharacterDataFromElement(line));
+
+		}
+	}
+
+	public static String getCharacterDataFromElement(Element e) {
+		if(e == null )return "";
+		Node child = e.getFirstChild();
+		if (child instanceof CharacterData) {
+			CharacterData cd = (CharacterData) child;
+			return cd.getData();
+		}
+		return "";
 	}
 }
